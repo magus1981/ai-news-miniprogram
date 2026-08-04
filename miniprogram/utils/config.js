c090d42ec6bdbc7d52840d6ba9a4ded4c42f410a
magus1981 <chenruipku@163.com>
2026-08-04 22:20:37 +0800

    feat: v1.1.0 - swipe day nav, tag follow, importance band filter, brand ??????

--- miniprogram/utils/config.js
+++ miniprogram/utils/config.js
@@ -1,4 +1,4 @@
-﻿/**
+/**
  * 鐜閰嶇疆
  * 寮€鍙戞椂鐢╨ocalhost锛岄儴缃插悗鏀逛负Vercel鍦板潃
  */
@@ -12,7 +12,7 @@ const CONFIG = {
     // 涓存椂鍩熷悕浼氳繃鏈燂細涓婁竴鏉￠毀閬撹窇浜嗙害12灏忔椂鍚庤繘绋嬭嚜宸遍€€浜嗭紝鍩熷悕璺熺潃娉ㄩ攢锛?
     // 鐜拌薄鏄皬绋嬪簭鎶?net::ERR_NAME_NOT_RESOLVED锛圖NS 瑙ｆ瀽涓嶄簡锛岃姹傛牴鏈病鍙戝嚭锛夛紱
     // 閲嶅缓闅ч亾鍚庡繀椤诲悓姝ユ澶勫苟閲嶆柊缂栬瘧銆備粎妯℃嫙鍣ㄥ彲鏀瑰洖 http://localhost:3000
-    apiBase: 'https://sega-measured-such-coupon.trycloudflare.com',
+    apiBase: 'http://121.40.116.248:3000',
   },
   prod: {
     // 閮ㄧ讲鍒癡ercel鍚庢浛鎹负瀹為檯鍦板潃
